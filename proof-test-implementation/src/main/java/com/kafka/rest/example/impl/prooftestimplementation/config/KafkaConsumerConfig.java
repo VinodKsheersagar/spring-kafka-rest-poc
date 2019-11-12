2bb2c406034804cd1c70bba3f434edd04dadc0d4
Vinod Kumar <ksheersagar.vinod@gmail.com>
2019-11-12 10:25:36 +0000

    updated consumer config class

--- proof-test-implementation/src/main/java/com/kafka/rest/example/impl/prooftestimplementation/config/KafkaConsumerConfig.java
+++ proof-test-implementation/src/main/java/com/kafka/rest/example/impl/prooftestimplementation/config/KafkaConsumerConfig.java
@@ -6,6 +6,7 @@ import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
